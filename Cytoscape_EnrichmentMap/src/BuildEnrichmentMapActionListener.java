461bd1877da520646fa88d1b57e23acfbe02fa1a
Ruth Isserlin <ruth.isserlin@utoronto.ca>
2009-01-28 17:02:15 +0000

    added a lot of features (mac multiple file update, modified jaccard, only one task, fixed visualizations, fixed a few bugs...)

--- Cytoscape_EnrichmentMap/src/BuildEnrichmentMapActionListener.java
+++ Cytoscape_EnrichmentMap/src/BuildEnrichmentMapActionListener.java
@@ -1,5 +1,8 @@
 import cytoscape.task.ui.JTaskConfig;
 import cytoscape.task.util.TaskManager;
+import cytoscape.task.TaskMonitor;
+import cytoscape.task.Task;
+
 
 import javax.swing.*;
 import java.awt.event.ActionListener;
@@ -19,18 +22,18 @@ public class BuildEnrichmentMapActionListener implements ActionListener {
     private InputFilesPanel inputPanel;
     private EnrichmentMapParameters params;
 
-    public BuildEnrichmentMapActionListener(InputFilesPanel inputPanel, EnrichmentMapParameters params) {
+    public BuildEnrichmentMapActionListener (InputFilesPanel inputPanel, EnrichmentMapParameters params) {
         this.inputPanel = inputPanel;
         this.params = params;
 
     }
 
-    public void actionPerformed(ActionEvent event) {
+   public void actionPerformed(ActionEvent event) {
 
-        GMTFileReader gmtFile;
-        GSEAResultFileReader gseaResultsFiles;
-        GCTFileReader gctFile;
-
+        config = new JTaskConfig();
+        config.displayCancelButton(true);
+        config.displayCloseButton(true);
+        config.displayStatus(true);
 
         //set the pvalue, qvalue, and jaccardCurOff
         double pvalue = this.inputPanel.getPvalue();
@@ -54,80 +57,10 @@ public class BuildEnrichmentMapActionListener implements ActionListener {
             params.setJaccardCutOff(jaccardCutOff);
         }
 
-
-        //Initialize a Task to perform all the tasks
-        //  Configure JTask
-        config = new JTaskConfig();
-
-        //  Show Cancel/Close Buttons
-        config.displayCancelButton(true);
-        config.displayStatus(true);
-
-        //Load in the GMT file
-        try{
-            //Load the GSEA geneset file
-            gmtFile = new GMTFileReader(params);
-            boolean success = TaskManager.executeTask(gmtFile, config);
-
-
-        } catch(Exception e){
-            JOptionPane.showMessageDialog(inputPanel,"unable to load GMT file");
-
-        }
-
-        //Load in the GCT file
-        try{
-            //Load the GSEA geneset file
-            gctFile = new GCTFileReader(params);
-            boolean success = TaskManager.executeTask(gctFile, config);
-
-
-        } catch(Exception e){
-            JOptionPane.showMessageDialog(inputPanel,"unable to load GSEA DATA (.GCT) file");
-
-        }
-
-        //trim the genesets to only contain the genes that are in the data file.
-        params.filterGenesets();
-
-        try{
-            //Load the GSEA result files
-            //Dataset1 (each dataset should have two files.)
-            GSEAResultFileReader gseaResultsFilesDataset1File1 = new GSEAResultFileReader(params, params.getGSEADataset1FileName1(), 1);
-            boolean success1a = TaskManager.executeTask(gseaResultsFilesDataset1File1, config);
-            GSEAResultFileReader gseaResultsFilesDataset1File2 = new GSEAResultFileReader(params, params.getGSEADataset1FileName2(), 1);
-            boolean success1b = TaskManager.executeTask(gseaResultsFilesDataset1File2, config);
-
-            //Load the second dataset only if there is a second dataset to load
-            if (params.isTwoDatasets()){
-                //Dataset2
-                GSEAResultFileReader gseaResultsFilesDataset2File1 = new GSEAResultFileReader(params, params.getGSEADataset2FileName1(), 2);
-                boolean success2a = TaskManager.executeTask(gseaResultsFilesDataset2File1, config);
-                GSEAResultFileReader gseaResultsFilesDataset2File2 = new GSEAResultFileReader(params, params.getGSEADataset2FileName2(), 2);
-                boolean success2b = TaskManager.executeTask(gseaResultsFilesDataset2File2, config);
-            }
-
-            //Initialize the set of genesets and GSEA results that we want to compute over
-            InitializeGenesetsOfInterest genesets_init = new InitializeGenesetsOfInterest(params);
-            boolean success4 = TaskManager.executeTask(genesets_init,config);
-
-            //compute the geneset similarities
-            ComputeSimilarity similarities = new ComputeSimilarity(params);
-            boolean success5 = TaskManager.executeTask(similarities,config);
-            HashMap similarity_results = similarities.getGeneset_similarities();
-
-            BuildEnrichmentMap map = new BuildEnrichmentMap(params,similarity_results);
-            boolean success3 = TaskManager.executeTask(map,config);
-
-            //close input panel
-            inputPanel.dispose();
-
-        } catch(Exception e){
-
-            JOptionPane.showMessageDialog(inputPanel,"unable to build map");
-
-        }
-
+        BuildGSEAEnrichmentMapTask new_map = new BuildGSEAEnrichmentMapTask(inputPanel,params);
+        boolean success = TaskManager.executeTask(new_map,config);
 
      }
+
+
 }
--- Cytoscape_EnrichmentMap/src/Enrichment_Map_Plugin.java
+++ Cytoscape_EnrichmentMap/src/Enrichment_Map_Plugin.java
@@ -17,66 +17,26 @@ public class Enrichment_Map_Plugin extends CytoscapePlugin {
       CONSTRUCTOR.
       --------------------------------------------------------------*/
     public Enrichment_Map_Plugin(){
+        //set-up menu options in plugins menu
+        JMenu menu = Cytoscape.getDesktop().getCyMenus().getOperationsMenu();
+        JMenuItem item;
 
-        //create a new action to respond to menu activation
-        Enrichment_Map_PluginAction action = new Enrichment_Map_PluginAction();
-        //set the preferred menu
-        action.setPreferredMenu("Plugins");
-        //and add it to the menus
-        Cytoscape.getDesktop().getCyMenus().addAction(action);
-		String tmp = System.getProperty("user.dir") ;
+        //Enrichment map submenu
+        JMenu submenu = new JMenu("Enrichment Maps");
+
+        //GSEA results panel panel
+        item = new JMenuItem("Load GSEA Results");
+        item.addActionListener(new LoadGSEAPanelAction());
+        submenu.add(item);
+
+
+        //Generic Results panel
+        item = new JMenuItem("Load Generic Results");
+        item.addActionListener(new LoadGenericPanelAction());
+        submenu.add(item);
+
+       menu.add(submenu);
     }
 
-     // Gives a description of this plugin.
-
-	public String describe() {
-        StringBuffer sb = new StringBuffer();
-            sb.append("Load GSEA results and Geneset file (gmt)  ");
-            sb.append("to generate an Enrichment map of the genesets where nodes are the genesets");
-            sb.append(" and edges are drawn between nodes that have common edges.");
-        return sb.toString();
-	}
-
-    // INTERNAL LISTENER-CLASS : This listener-class gets attached to the menu item.
-
-	public class Enrichment_Map_PluginAction extends CytoscapeAction {
-
-
-        // The constructor sets the text that should appear on the menu item.
-
-    	public Enrichment_Map_PluginAction() {super("Enrichment Maps");}
-
-        /**
-         * This method opens the BiNGO settingspanel upon selection of the menu item
-         * and opens the settingspanel for BiNGO.
-         *
-         * @param event event triggered when BiNGO menu item clicked.
-         */
-
-        public void actionPerformed(ActionEvent event) {
-
-             //create new window to prompt users to load their files
-            JFrame window = new JFrame("Enrichment Map Settings");
-
-            // open new dialog
-		    InputFilesPanel amd
-		        = new InputFilesPanel(Cytoscape.getDesktop(),
-						      true);
-
-            amd.pack();
-		    amd.setLocationRelativeTo(Cytoscape.getDesktop());
-            amd.setVisible(true);
-
-             //  Configure JTask
-            JTaskConfig config = new JTaskConfig();
-
-            //  Show Cancel/Close Buttons
-            config.displayCancelButton(true);
-            config.displayStatus(true);
-
-            
-
-		}
-    }
 }
 
