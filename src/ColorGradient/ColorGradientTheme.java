dcf01166bb9f6021e02e59b714b9a9c689a25685
Ruth Isserlin <ruth.isserlin@utoronto.ca>
2009-02-09 21:52:32 +0000

    Changed the expression viewing to be in the cytopanel (added tab to attribute viewer panel) and moved the geneset list up to the side cyotpanel.  Also added a summary of the parameters to the side panel.  Incorporated modrian's color gradient and got rid of the legend I created last week.

---
+++ src/ColorGradient/ColorGradientTheme.java
@@ -0,0 +1,185 @@
+// $Id: ColorGradientTheme.java,v 1.1 2008/06/06 15:30:13 grossb Exp $
+//------------------------------------------------------------------------------
+/** Copyright (c) 2008 Memorial Sloan-Kettering Cancer Center.
+ **
+ ** This library is free software; you can redistribute it and/or modify it
+ ** under the terms of the GNU Lesser General Public License as published
+ ** by the Free Software Foundation; either version 2.1 of the License, or
+ ** any later version.
+ **
+ ** This library is distributed in the hope that it will be useful, but
+ ** WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
+ ** MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
+ ** documentation provided hereunder is on an "as is" basis, and
+ ** Memorial Sloan-Kettering Cancer Center
+ ** has no obligations to provide maintenance, support,
+ ** updates, enhancements or modifications.  In no event shall
+ ** Memorial Sloan-Kettering Cancer Center
+ ** be liable to any party for direct, indirect, special,
+ ** incidental or consequential damages, including lost profits, arising
+ ** out of the use of this software and its documentation, even if
+ ** Memorial Sloan-Kettering Cancer Center
+ ** has been advised of the possibility of such damage.  See
+ ** the GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this library; if not, write to the Free Software Foundation,
+ ** Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ **/
+
+
+// imports
+import java.awt.Color;
+
+/**
+ * Encapsulates a Color Theme, e.g. a BLUE_RED_GRADIENT_THEME.
+ *
+ * @author Ethan Cerami.
+ */
+public class ColorGradientTheme {
+    private String name;
+    private Color minColor, centerColor, maxColor;
+    private Color noDataColor;
+	private Color minLabelColor, maxLabelColor;
+
+    /**
+     * Private Constructor.  Enforces Enumeration Pattern.
+     *
+     * @param name        Name of Color Theme.
+     * @param minColor    Minimum Color;  lower bound of color gradient.
+     * @param centerColor Center Color; center of color gradient.
+     * @param maxColor    Maxiumum Color; upper bound of color gradient.
+     * @param noDataColor No Data Color
+	 * @param minLabelColor Color
+	 * @param maxLabelColor Color
+     */
+    private ColorGradientTheme(String name, Color minColor, Color centerColor,
+							   Color maxColor, Color noDataColor,
+							   Color minLabelColor, Color maxLabelColor) {
+        this.name = name;
+        this.minColor = minColor;
+        this.centerColor = centerColor;
+        this.maxColor = maxColor;
+        this.noDataColor = noDataColor;
+		this.minLabelColor = minLabelColor;
+		this.maxLabelColor = maxLabelColor;
+    }
+
+    /**
+     * Gets String representation of Color Theme.
+     * Returned value includes name, and all colors.
+     *
+     * @return String representation of Color Theme.
+     */
+    public String toString() {
+        return "Color Theme:  " + this.name + " [Min Color:  " + minColor
+                + ", Center Color:  " + centerColor + ", Max Color:  "
+                + maxColor + ", No Data Color" + noDataColor + "]";
+    }
+
+    /**
+     * Gets Name of Color Theme.
+     *
+     * @return Name of Color Theme.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Gets the color associated with the minimum value.
+     * This represents the lower bounds of the color gradient.
+     *
+     * @return Color Object.
+     */
+    public Color getMinColor() {
+        return this.minColor;
+    }
+
+    /**
+     * Gets the color associated with the center value.
+     * This represents the center of the color gradient.
+     *
+     * @return Color Object.
+     */
+    public Color getCenterColor() {
+        return this.centerColor;
+    }
+
+    /**
+     * Gets the color associated with the maximum value.
+     * This represents the upper bounds of the color gradient.
+     *
+     * @return Color Object.
+     */
+    public Color getMaxColor() {
+        return this.maxColor;
+    }
+
+    /**
+     * Gets the Color Associated with Node Data.
+     * For example, if a node has no expression data, it is colored with
+     * the noDataColor Object.
+     *
+     * @return Color Object.
+     */
+    public Color getNoDataColor() {
+        return this.noDataColor;
+    }
+
+    /**
+     * Gets the Label color when gradient is at the low end.
+     *
+     * @return Color Object.
+     */
+    public Color getMinLabelColor() {
+        return this.minLabelColor;
+    }
+
+    /**
+     * Gets the Label color when gradient is at the high end.
+     *
+     * @return Color Object.
+     */
+    public Color getMaxLabelColor() {
+        return this.maxLabelColor;
+    }
+
+    /**
+     * Color Theme:  BLUE_RED_GRADIENT_THEME
+     */
+    public static final ColorGradientTheme BLUE_RED_GRADIENT_THEME
+            = new ColorGradientTheme("Blue Red", Color.BLUE, Color.WHITE,
+									 Color.RED, Color.LIGHT_GRAY,
+									 Color.WHITE, Color.BLACK);
+
+    /**
+     * Color Theme:  GREEN_RED_GRADIENT_THEME
+     */
+    public static final ColorGradientTheme GREEN_RED_GRADIENT_THEME
+            = new ColorGradientTheme("Green Red", Color.GREEN, Color.BLACK,
+									 Color.RED, Color.LIGHT_GRAY,
+									 Color.BLACK, Color.WHITE);
+
+    /**
+     * Color Theme:  YELLOW_BLUE_GRADIENT_THEME
+     */
+    public static final ColorGradientTheme YELLOW_BLUE_GRADIENT_THEME
+            = new ColorGradientTheme("Yellow Blue", Color.YELLOW, Color.BLACK,
+									 Color.BLUE, Color.LIGHT_GRAY,
+									 Color.BLACK, Color.WHITE);
+      /**
+     * Color Theme:  GREEN_MAGENTA_GRADIENT_THEME
+     */
+    public static final ColorGradientTheme GREEN_MAGENTA_GRADIENT_THEME
+            = new ColorGradientTheme("Green Magenta", Color.GREEN, Color.WHITE,
+									 Color.MAGENTA, Color.LIGHT_GRAY,
+									 Color.BLACK, Color.WHITE);
+    /**
+     * Color Theme:  RED_ONECOLOR_GRADIENT_THEME
+     */
+    public static final ColorGradientTheme RED_ONECOLOR_GRADIENT_THEME
+            = new ColorGradientTheme("Red", Color.WHITE, new Color(255,120,120),
+									 Color.RED, Color.LIGHT_GRAY,
+									 Color.BLACK, Color.WHITE);
+}
---
+++ src/ParametersPanel.java
@@ -0,0 +1,85 @@
+import cytoscape.visual.mappings.*;
+import cytoscape.visual.VisualPropertyType;
+import cytoscape.Cytoscape;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+/**
+ * Created by
+ * User: risserlin
+ * Date: Feb 9, 2009
+ * Time: 2:41:48 PM
+ */
+public class ParametersPanel extends JPanel {
+    private JTextPane runInfo;
+    private EnrichmentMapParameters params;
+
+    public ParametersPanel(EnrichmentMapParameters params) {
+           this.setLayout(new java.awt.BorderLayout());
+
+           this.params = params;
+           //information about the current analysis
+           runInfo = new JTextPane();
+           runInfo.setEditable(false);
+           runInfo.setContentType("text/html");
+           runInfo.setText(getRunInfo());
+
+           this.add(runInfo);
+           //this.add(createLegend());
+
+       }
+
+       private String getRunInfo(){
+
+           String runInfoText = "<html> <h1>Parameters:</h1>";
+           runInfoText = runInfoText + "<b>P-value Cut-off:</b>" + params.getPvalue() + "<br>";
+           runInfoText = runInfoText + "<b>FDR Q-value Cut-off:</b>" + params.getQvalue() + "<br>";
+           runInfoText = runInfoText + "<b>Jaccard Cut-off:</b>" + params.getJaccardCutOff() + "<br>";
+           if(params.isJaccard()){
+                runInfoText = runInfoText + "<b>Test used:</b>  Jaccard Index<br>";
+           }
+           else{
+                runInfoText = runInfoText + "<b>Test used:</b>  Overlap Index<br>";
+           }
+           runInfoText = runInfoText + "<font size=-1><b>Genesets File:</b>" + params.getGMTFileName() + "<br>";
+           runInfoText = runInfoText + "<b>Dataset 1 Data Files:</b> " + params.getEnrichmentDataset1FileName1() + ",<br>" + params.getEnrichmentDataset1FileName2() + "<br>";
+           if(params.isTwoDatasets()){
+               runInfoText = runInfoText + "<b>Dataset 2 Data Files:</b> " + params.getEnrichmentDataset2FileName1() + ",<br>" + params.getEnrichmentDataset2FileName2() + "<br>";
+           }
+           if(params.isData()){
+               runInfoText = runInfoText + "<b>Data file:</b>" + params.getGCTFileName1() + "<br>";
+           }
+           if(params.isData2()){
+               runInfoText = runInfoText + "<b>Data file 2:</b>" + params.getGCTFileName2() + "<br>";
+           }
+
+           runInfoText = runInfoText + "</font></html>";
+           return runInfoText;
+       }
+
+    private JPanel createLegend(){
+
+        JPanel legends = new JPanel();
+
+        //create a legend based on the colouring of the nodes
+        ContinuousMapping continuousMapping = new ContinuousMapping(Color.WHITE, ObjectMapping.NODE_MAPPING);
+        Interpolator numToColor = new LinearNumberToColorInterpolator();
+        continuousMapping.setInterpolator(numToColor);
+
+        JPanel legend1  = continuousMapping.getLegend(VisualPropertyType.NODE_FILL_COLOR);
+
+        legends.add(legend1);
+
+        if(params.isTwoDatasets()){
+            continuousMapping.setControllingAttributeName("NODE_BORDER_COLOR", Cytoscape.getCurrentNetwork(),true);
+            JPanel legend2 =continuousMapping.getLegend(VisualPropertyType.NODE_BORDER_COLOR);
+            legends.add(legend2);
+        }
+        return legends;
+
+
+    }
+
+}
