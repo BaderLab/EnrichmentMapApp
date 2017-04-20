b733c5c036314f835f7c56d250494d4532899089
Christian Lopes <chrtannus@gmail.com>
2017-04-20 17:45:42 -0400

    Changes the regular node colour to light grey, so it does not interfere with chart colours.

--- EnrichmentMapPlugin/src/main/java/org/baderlab/csplugins/enrichmentmap/style/EMStyleBuilder.java
+++ EnrichmentMapPlugin/src/main/java/org/baderlab/csplugins/enrichmentmap/style/EMStyleBuilder.java
@@ -130,8 +130,8 @@ public class EMStyleBuilder {
 
 	public static class Colors {
 		// See http://colorbrewer2.org/#type=diverging&scheme=RdBu&n=6
-		public static final Color DEF_NODE_BORDER_COLOR = new Color(33, 102, 172);
-		public static final Color DEF_NODE_COLOR = new Color(209, 229, 240);
+		public static final Color DEF_NODE_BORDER_COLOR = new Color(148, 148, 148);
+		public static final Color DEF_NODE_COLOR = new Color(240, 240, 240);
 		public static final Color SIG_NODE_BORDER_COLOR = new Color(239, 138, 98);
 		public static final Color SIG_NODE_COLOR = new Color(253, 219, 199);
 
