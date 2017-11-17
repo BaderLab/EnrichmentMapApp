43b91ca0cbe368e403ed6e0a2286a3cbb8cdbcaf
Mike Kucera <mikekucera@gmail.com>
2017-11-17 16:05:18 -0500

    Mann-whit ranks, use combo boxes instead of table. Refs #288

--- EnrichmentMapPlugin/src/main/java/org/baderlab/csplugins/enrichmentmap/view/postanalysis2/MannWhitRanksPanel.java
+++ EnrichmentMapPlugin/src/main/java/org/baderlab/csplugins/enrichmentmap/view/postanalysis2/MannWhitRanksPanel.java
@@ -1,28 +1,28 @@
 package org.baderlab.csplugins.enrichmentmap.view.postanalysis2;
 
+import static java.awt.GridBagConstraints.EAST;
+import static java.awt.GridBagConstraints.NONE;
+
 import java.awt.BorderLayout;
-import java.util.ArrayList;
+import java.awt.GridBagLayout;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.swing.DefaultCellEditor;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableCellEditor;
 
 import org.baderlab.csplugins.enrichmentmap.model.EMDataSet;
 import org.baderlab.csplugins.enrichmentmap.model.EnrichmentMap;
+import org.baderlab.csplugins.enrichmentmap.view.util.GBCFactory;
 import org.baderlab.csplugins.enrichmentmap.view.util.SwingUtil;
 
 @SuppressWarnings("serial")
 public class MannWhitRanksPanel extends JPanel {
 
-	private JTable table;
+	private Map<String,String> results = new HashMap<>();
 	
 	public MannWhitRanksPanel(EnrichmentMap map) {
 		createContents(map);
@@ -32,36 +32,41 @@ public class MannWhitRanksPanel extends JPanel {
 		JLabel title = new JLabel("Ranks to use for Mann-Whitney test");
 		SwingUtil.makeSmall(title);
 		
-		table = new JTable() {
-			@Override
-			public TableCellEditor getCellEditor(int row, int col) {
-				int modelCol = convertColumnIndexToModel(col);
-				int modelRow = convertRowIndexToModel(row);
-				if(col == 1) {
-					MannWhitTableModel model = (MannWhitTableModel)table.getModel();
-					EMDataSet dataSet = model.getDataSet(modelRow);
-					JComboBox<String> combo = new JComboBox<>();
-					for(String ranking : dataSet.getAllRanksNames()) {
-						combo.addItem(ranking);
-					}
-					combo.setSelectedItem(model.getValueAt(modelRow, modelCol));
-					return new DefaultCellEditor(combo);
-				} else {
-					return super.getCellEditor(row, col);
-				}
+		List<EMDataSet> dataSets = map.getDataSetList();
+
+		JPanel body = new JPanel(new GridBagLayout());
+		int y = 0;
+		for(EMDataSet dataset : dataSets) {
+			final String dataSetName = dataset.getName();
+			JLabel label = new JLabel(dataSetName + ":");
+			JComboBox<String> combo = new JComboBox<>();
+			for(String ranksName : dataset.getAllRanksNames()) {
+				combo.addItem(ranksName);
 			}
-		};
+			SwingUtil.makeSmall(label, combo);
+			if(combo.getItemCount() <= 1) {
+				combo.setEnabled(false);
+			}
+			
+			combo.addActionListener(e -> {
+				String ranks = combo.getSelectedItem().toString();
+				results.put(dataSetName, ranks);
+			});
+			
+			results.put(dataSetName, combo.getSelectedItem().toString());
+			
+			body.add(label, GBCFactory.grid(0,y).weightx(.5).anchor(EAST).fill(NONE).get());
+			body.add(combo, GBCFactory.grid(1,y).weightx(.5).get());
+			y++;
+		}
 		
-		JScrollPane scrollPane = new JScrollPane(table);
+		JPanel container = new JPanel(new BorderLayout());
+		container.add(body, BorderLayout.NORTH);
+		
+		JScrollPane scrollPane = new JScrollPane(container);
+		scrollPane.setAlignmentX(TOP_ALIGNMENT);
 		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		table.setFillsViewportHeight(true);
-		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-		table.setCellSelectionEnabled(false);
-		table.setRowSelectionAllowed(false);
-		table.setAutoCreateRowSorter(false);
-
-		table.setModel(new MannWhitTableModel(map));
 		
 		setLayout(new BorderLayout());
 		add(title, BorderLayout.NORTH);
@@ -70,78 +75,7 @@ public class MannWhitRanksPanel extends JPanel {
 	}
 	
 	public Map<String,String> getResults() {
-		MannWhitTableModel model = (MannWhitTableModel)table.getModel();
-		Map<String,String> results = new HashMap<>();
-		for(int row = 0; row < model.getRowCount(); row++) {
-			EMDataSet dataSet = model.getDataSet(row);
-			String ranking = model.getRanking(row);
-			results.put(dataSet.getName(), ranking);
-		}
 		return results;
-	}
-	
-	private static class MannWhitTableModel extends AbstractTableModel {
-		
-		List<EMDataSet> dataSets;
-		List<String> rankingNames;
-		
-		public MannWhitTableModel(EnrichmentMap map) {
-			dataSets = map.getDataSetList();
-			rankingNames = new ArrayList<>(dataSets.size());
-			for(EMDataSet dataSet : dataSets) {
-				String ranking = dataSet.getAllRanksNames().iterator().next();
-				rankingNames.add(ranking);
-			}
-		}
-		
-		@Override
-		public String getColumnName(int col) {
-			if(col == 0)
-				return "Data Set";
-			if(col == 1)
-				return "Ranks";
-			return null;
-		}
-
-		@Override
-		public int getRowCount() {
-			return dataSets.size();
-		}
-		
-		public EMDataSet getDataSet(int row) {
-			return dataSets.get(row);
-		}
-		
-		public String getRanking(int row) {
-			return rankingNames.get(row);
-		}
-
-		@Override
-		public int getColumnCount() {
-			return 2;
-		}
-
-		@Override
-		public boolean isCellEditable(int row, int col) {
-			return col == 1;
-		}
-		
-		@Override
-		public Object getValueAt(int row, int col) {
-			if(col == 0)
-				return getDataSet(row).getName();
-			if(col == 1)
-				return getRanking(row);
-			return null;
-		}
-		
-		public void setValueAt(Object value, int row, int col) {
-			if(col == 1) {
-				String ranking = value.toString();
-				rankingNames.set(row, ranking);
-			}
-		}
-		
 	}
 
 }
