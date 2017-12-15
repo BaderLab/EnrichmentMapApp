bdcef3173aba182588b0804fd9a7f6741234eff0
Mike Kucera <mikekucera@gmail.com>
2017-12-15 15:59:50 -0500

    Better expression caching for heatmap. Refs #294

--- EnrichmentMapPlugin/src/main/java/org/baderlab/csplugins/enrichmentmap/view/heatmap/table/HeatMapTableModel.java
+++ EnrichmentMapPlugin/src/main/java/org/baderlab/csplugins/enrichmentmap/view/heatmap/table/HeatMapTableModel.java
@@ -9,9 +9,7 @@ import java.util.NavigableMap;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.TreeMap;
-import java.util.concurrent.ExecutionException;
 
-import javax.annotation.Nullable;
 import javax.swing.table.AbstractTableModel;
 
 import org.apache.commons.lang3.tuple.Pair;
@@ -22,9 +20,6 @@ import org.baderlab.csplugins.enrichmentmap.model.GeneExpressionMatrix;
 import org.baderlab.csplugins.enrichmentmap.model.SetOfEnrichmentResults;
 import org.baderlab.csplugins.enrichmentmap.view.heatmap.HeatMapParams.Compress;
 import org.baderlab.csplugins.enrichmentmap.view.heatmap.HeatMapParams.Transform;
-
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 
 
 
@@ -41,217 +36,13 @@ public class HeatMapTableModel extends AbstractTableModel {
 	
 	private final List<EMDataSet> datasets;
 	private Map<Compress,ExpressionData> data = new EnumMap<>(Compress.class);
+	private ExpressionCache expressionCache;
 	
 	private List<String> genes;
 	private Transform transform;
 	private Compress compress;
 	private Map<Integer,RankValue> ranking;
 	private String ranksColName = "Ranks";
-
-	
-	private interface ExpressionData {
-		EMDataSet getDataSet(int col);
-		double getValue(int geneID, int col);
-		String getColumnName(int col);
-		public default Optional<String> getPhenotype(int col) { return Optional.empty(); };
-		int getSize();
-		public default void transformChanged() {};
-	}
-	
-	
-	private class Uncompressed implements ExpressionData {
-
-		// for Compress.NONE, but storing the expression data again would use too much memory, so we access through the EMDataSet object
-		private final NavigableMap<Integer,EMDataSet> colToDataSet = new TreeMap<>();
-		private final int expressionCount;
-		private final Cache<Pair<Integer,EMDataSet>, float[]> cache;
-		
-		public Uncompressed(List<EMDataSet> datasets) {
-			int rangeFloor = DESC_COL_COUNT;
-			colToDataSet.put(0, null);
-			for(EMDataSet dataset : datasets) {
-				GeneExpressionMatrix matrix = dataset.getExpressionSets();
-				colToDataSet.put(rangeFloor, dataset);
-				rangeFloor += matrix.getNumConditions() - 2;
-			}
-			expressionCount = rangeFloor - DESC_COL_COUNT;
-			
-			cache = CacheBuilder.newBuilder().maximumSize(20).build();
-		}
-		
-		public EMDataSet getDataSet(int col) {
-			return colToDataSet.floorEntry(col).getValue();
-		}
-		
-		private int getIndexInDataSet(int col) {
-			int start = colToDataSet.floorKey(col);
-			return col - start;
-		}
-		
-		@Override
-		public double getValue(int geneID, int col) { // col value 
-			EMDataSet dataset = getDataSet(col);
-			
-			try {
-				float vals[] = cache.get(Pair.of(geneID, dataset), () -> getExpression(dataset, geneID, transform));
-				return vals == null ? Double.NaN : (double) vals[getIndexInDataSet(col)];
-			} catch (ExecutionException e) {
-				return Double.NaN;
-			}
-		}
-
-		@Override
-		public String getColumnName(int col) {
-			EMDataSet dataset = getDataSet(col);
-			String[] columns = dataset.getExpressionSets().getColumnNames();
-			int index = getIndexInDataSet(col) + 2;
-			return columns[index];
-		}
-
-		@Override
-		public int getSize() {
-			return expressionCount;
-		}
-
-		@Override
-		public Optional<String> getPhenotype(int col) {
-			EMDataSet dataset = getDataSet(col);
-			int index = getIndexInDataSet(col);
-			String[] classes = dataset.getEnrichments().getPhenotypes();
-			if(classes != null && index < classes.length) {
-				return Optional.ofNullable(classes[index]);
-			}
-			return Optional.empty();
-		}
-		
-		public void transformChanged() {
-			cache.invalidateAll();
-		}
-	}
-	
-	
-	
-	private class CompressDataset implements ExpressionData {
-		private final List<EMDataSet> datasets;
-		
-		public CompressDataset(List<EMDataSet> datasets) {
-			this.datasets = datasets;
-		}
-		
-		public EMDataSet getDataSet(int col) {
-			return datasets.get(col-DESC_COL_COUNT);
-		}
-		
-		@Override
-		public double getValue(int geneID, int col) {
-			EMDataSet dataset = getDataSet(col);
-			return (double) getCompressedExpression(dataset, geneID);
-		}
-
-		@Override
-		public String getColumnName(int col) {
-			EMDataSet dataset = getDataSet(col);
-			if(map.isDistinctExpressionSets())
-				return dataset.getName();
-			else
-				return "Expressions";
-		}
-
-		@Override
-		public int getSize() {
-			return datasets.size();
-		}
-		
-		private float getCompressedExpression(EMDataSet dataset, int geneID) {
-			float[] expression = getExpression(dataset, geneID, transform);
-			if(expression == null)
-				return Float.NaN;
-			
-			switch(compress) {
-				case DATASET_MEDIAN:	return GeneExpression.median(expression);
-				case DATASET_MAX:	return GeneExpression.max(expression);
-				case DATASET_MIN:	return GeneExpression.min(expression);
-				default:				return Float.NaN;
-			}
-		}
-	}
-	
-	
-	
-	private class CompressClass implements ExpressionData {
-
-		private List<Pair<EMDataSet,String>> headers = new ArrayList<>();
-		
-		public CompressClass(List<EMDataSet> datasets) {
-			for(EMDataSet dataset : datasets) {
-				SetOfEnrichmentResults enrichments = dataset.getEnrichments();
-				String pheno1 = enrichments.getPhenotype1();
-				String pheno2 = enrichments.getPhenotype2();
-				if(pheno1 != null)
-					headers.add(Pair.of(dataset, pheno1));
-				if(pheno2 != null)
-					headers.add(Pair.of(dataset, pheno2));
-			}
-		}
-		
-		@Override
-		public EMDataSet getDataSet(int col) {
-			return headers.get(col-DESC_COL_COUNT).getLeft();
-		}
-
-		@Override
-		public String getColumnName(int col) {
-			return headers.get(col-DESC_COL_COUNT).getRight();
-		}
-		
-		@Override
-		public Optional<String> getPhenotype(int col) {
-			return Optional.of(getColumnName(col));
-		}
-		
-		@Override
-		public double getValue(int geneID, int col) {
-			EMDataSet dataset = getDataSet(col);
-			String pheno = getColumnName(col);
-			
-			String[] phenotypes = dataset.getEnrichments().getPhenotypes();
-			if(phenotypes == null || phenotypes.length == 0)
-				return Double.NaN;
-			
-			float[] expressions = getExpression(dataset, geneID, transform); 
-			if(expressions == null || expressions.length == 0 || expressions.length != phenotypes.length)
-				return Double.NaN;
-			
-			int size = 0;
-			for(int i = 0; i < expressions.length; i++) {
-				if(pheno.equals(phenotypes[i])) {
-					size++;
-				}
-			}
-			
-			float[] vals = new float[size];
-			int vi = 0;
-			
-			for(int i = 0; i < expressions.length; i++) {
-				if(pheno.equals(phenotypes[i])) {
-					vals[vi++] = expressions[i];
-				}
-			}
-			
-			switch(compress) {
-				case CLASS_MEDIAN: return GeneExpression.median(vals);
-				case CLASS_MAX:    return GeneExpression.max(vals);
-				case CLASS_MIN:    return GeneExpression.min(vals);
-				default:	           return Double.NaN;
-			}
-		}
-
-		@Override
-		public int getSize() {
-			return headers.size();
-		}
-		
-	}
 	
 	
 	public HeatMapTableModel(EnrichmentMap map, Map<Integer,RankValue> ranking, List<String> genes, Transform transform, Compress compress) {
@@ -278,6 +69,8 @@ public class HeatMapTableModel extends AbstractTableModel {
 		data.put(Compress.CLASS_MEDIAN, compressClass);
 		data.put(Compress.CLASS_MAX, compressClass);
 		data.put(Compress.CLASS_MIN, compressClass);
+		
+		expressionCache = new ExpressionCache(transform);
 	}
 	
 	
@@ -289,7 +82,7 @@ public class HeatMapTableModel extends AbstractTableModel {
 		boolean structureChanged = !this.compress.sameStructure(compress);
 		this.transform = transform;
 		this.compress = compress;
-		data.values().forEach(ExpressionData::transformChanged);
+		this.expressionCache = new ExpressionCache(transform);
 		if(structureChanged)
 			fireTableStructureChanged();
 		fireTableDataChanged();
@@ -409,15 +202,196 @@ public class HeatMapTableModel extends AbstractTableModel {
 		return row;
 	}
 	
-	private static @Nullable float[] getExpression(EMDataSet dataset, int geneID, Transform transform) {
-		GeneExpression expression = getGeneExpression(dataset, geneID);
-		if(expression != null) {
-			switch(transform) {
-				case ROW_NORMALIZE: return expression.rowNormalize();
-				case LOG_TRANSFORM: return expression.rowLogTransform();
-				case AS_IS:         return expression.getExpression();
+	
+	/**
+	 * Common interface for different levels of compression.
+	 */
+	private interface ExpressionData {
+		EMDataSet getDataSet(int col);
+		double getValue(int geneID, int col);
+		String getColumnName(int col);
+		public default Optional<String> getPhenotype(int col) { return Optional.empty(); };
+		int getSize();
+	}
+	
+	
+	private class Uncompressed implements ExpressionData {
+
+		private final NavigableMap<Integer,EMDataSet> colToDataSet = new TreeMap<>();
+		private final int expressionCount;
+		
+		public Uncompressed(List<EMDataSet> datasets) {
+			int rangeFloor = DESC_COL_COUNT;
+			colToDataSet.put(0, null);
+			for(EMDataSet dataset : datasets) {
+				GeneExpressionMatrix matrix = dataset.getExpressionSets();
+				colToDataSet.put(rangeFloor, dataset);
+				rangeFloor += matrix.getNumConditions() - 2;
+			}
+			expressionCount = rangeFloor - DESC_COL_COUNT;
+		}
+		
+		public EMDataSet getDataSet(int col) {
+			return colToDataSet.floorEntry(col).getValue();
+		}
+		
+		private int getIndexInDataSet(int col) {
+			int start = colToDataSet.floorKey(col);
+			return col - start;
+		}
+		
+		@Override
+		public double getValue(int geneID, int col) { // col value 
+			EMDataSet dataset = getDataSet(col);
+			return expressionCache.getExpression(dataset, geneID, getIndexInDataSet(col));
+		}
+
+		@Override
+		public String getColumnName(int col) {
+			EMDataSet dataset = getDataSet(col);
+			String[] columns = dataset.getExpressionSets().getColumnNames();
+			int index = getIndexInDataSet(col) + 2;
+			return columns[index];
+		}
+
+		@Override
+		public int getSize() {
+			return expressionCount;
+		}
+
+		@Override
+		public Optional<String> getPhenotype(int col) {
+			EMDataSet dataset = getDataSet(col);
+			int index = getIndexInDataSet(col);
+			String[] classes = dataset.getEnrichments().getPhenotypes();
+			if(classes != null && index < classes.length) {
+				return Optional.ofNullable(classes[index]);
+			}
+			return Optional.empty();
+		}
+	}
+	
+	
+	
+	private class CompressDataset implements ExpressionData {
+		private final List<EMDataSet> datasets;
+		
+		public CompressDataset(List<EMDataSet> datasets) {
+			this.datasets = datasets;
+		}
+		
+		public EMDataSet getDataSet(int col) {
+			return datasets.get(col-DESC_COL_COUNT);
+		}
+		
+		@Override
+		public double getValue(int geneID, int col) {
+			EMDataSet dataset = getDataSet(col);
+			return (double) getCompressedExpression(dataset, geneID);
+		}
+
+		@Override
+		public String getColumnName(int col) {
+			EMDataSet dataset = getDataSet(col);
+			return map.isDistinctExpressionSets() ? dataset.getName() : "Expressions";
+		}
+
+		@Override
+		public int getSize() {
+			return datasets.size();
+		}
+		
+		private float getCompressedExpression(EMDataSet dataset, int geneID) {
+			Optional<float[]> expression = expressionCache.getExpressions(dataset, geneID);
+			if(!expression.isPresent())
+				return Float.NaN;
+			
+			switch(compress) {
+				case DATASET_MEDIAN:	return GeneExpression.median(expression.get());
+				case DATASET_MAX:	return GeneExpression.max(expression.get());
+				case DATASET_MIN:	return GeneExpression.min(expression.get());
+				default:				return Float.NaN;
 			}
 		}
-		return null;
+	}
+	
+	
+	
+	private class CompressClass implements ExpressionData {
+
+		private List<Pair<EMDataSet,String>> headers = new ArrayList<>();
+		
+		public CompressClass(List<EMDataSet> datasets) {
+			for(EMDataSet dataset : datasets) {
+				SetOfEnrichmentResults enrichments = dataset.getEnrichments();
+				String pheno1 = enrichments.getPhenotype1();
+				String pheno2 = enrichments.getPhenotype2();
+				if(pheno1 != null)
+					headers.add(Pair.of(dataset, pheno1));
+				if(pheno2 != null)
+					headers.add(Pair.of(dataset, pheno2));
+			}
+		}
+		
+		@Override
+		public EMDataSet getDataSet(int col) {
+			return headers.get(col-DESC_COL_COUNT).getLeft();
+		}
+
+		@Override
+		public String getColumnName(int col) {
+			return headers.get(col-DESC_COL_COUNT).getRight();
+		}
+		
+		@Override
+		public Optional<String> getPhenotype(int col) {
+			return Optional.of(getColumnName(col));
+		}
+		
+		@Override
+		public double getValue(int geneID, int col) {
+			EMDataSet dataset = getDataSet(col);
+			String pheno = getColumnName(col);
+			
+			String[] phenotypes = dataset.getEnrichments().getPhenotypes();
+			if(phenotypes == null || phenotypes.length == 0)
+				return Double.NaN;
+			
+			Optional<float[]> optExpr = expressionCache.getExpressions(dataset, geneID); 
+			if(!optExpr.isPresent())
+				return Double.NaN;
+			float[] expressions = optExpr.get();
+			if(expressions.length == 0 || expressions.length != phenotypes.length)
+				return Double.NaN;
+			
+			int size = 0;
+			for(int i = 0; i < expressions.length; i++) {
+				if(pheno.equals(phenotypes[i])) {
+					size++;
+				}
+			}
+			
+			float[] vals = new float[size];
+			int vi = 0;
+			
+			for(int i = 0; i < expressions.length; i++) {
+				if(pheno.equals(phenotypes[i])) {
+					vals[vi++] = expressions[i];
+				}
+			}
+			
+			switch(compress) {
+				case CLASS_MEDIAN: return GeneExpression.median(vals);
+				case CLASS_MAX:    return GeneExpression.max(vals);
+				case CLASS_MIN:    return GeneExpression.min(vals);
+				default:	           return Double.NaN;
+			}
+		}
+
+		@Override
+		public int getSize() {
+			return headers.size();
+		}
+		
 	}
 }
