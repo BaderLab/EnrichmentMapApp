2919dcaf89ae63b035028a1920d12e7cf1c33dc4
Mike Kucera <mikekucera@gmail.com>
2023-07-26 14:19:07 -0400

    Set menu gravity on action

--- EnrichmentMapPlugin/src/main/java/org/baderlab/csplugins/enrichmentmap/actions/OpenEnrichmentMapAction.java
+++ EnrichmentMapPlugin/src/main/java/org/baderlab/csplugins/enrichmentmap/actions/OpenEnrichmentMapAction.java
@@ -26,6 +26,7 @@ public class OpenEnrichmentMapAction extends AbstractCyAction implements Task {
 	public OpenEnrichmentMapAction() {
 		super(NAME);
 		setPreferredMenu("Apps");
+		setMenuGravity(3.1f);
 	}
 
 	public synchronized void showPanels() {
